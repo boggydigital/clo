1aec28a1a43712d93eb79efec73ec4a23a71de97
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2020-12-30 23:41:17 -0800

    More test coverage

--- internal/tokens_test.go
+++ internal/tokens_test.go
@@ -36,3 +36,70 @@ func TestTokenString(t *testing.T) {
 		})
 	}
 }
+
+func TestNext(t *testing.T) {
+	tests := []struct {
+		tokenType int
+		nextLen   int
+	}{
+		{command, 5},
+		{commandAbbr, 5},
+		{argument, 6},
+		{argumentAbbr, 6},
+		{valueFixed, 5},
+		{valueDefault, 5},
+		{value, 5},
+		{flag, 2},
+		{flagAbbr, 2},
+		{-1, 0},
+		{math.MaxInt64, 0},
+	}
+
+	for _, tt := range tests {
+		t.Run(tokenString(tt.tokenType), func(t *testing.T) {
+			if len(next(tt.tokenType)) != tt.nextLen {
+				t.Error("unexpected length of the next token types")
+			}
+		})
+	}
+}
+
+func TestFirst(t *testing.T) {
+	if len(first()) != 2 {
+		t.Error("expected 2 tokens to be the first possible")
+	}
+}
+
+func TestExpandAbbr(t *testing.T) {
+	tests := []struct {
+		token     string
+		expToken  string
+		tokenType int
+		errorExp  bool
+	}{
+		{"c1", "command1", commandAbbr, false},
+		{"command-abbr-that-doesnt-exist", "", commandAbbr, true},
+		{"a1", "argument1", argumentAbbr, false},
+		{"argument-abbr-that-doesnt-exist", "", argumentAbbr, true},
+		{"f1", "flag1", flagAbbr, false},
+		{"flag-abbr-that-doesnt-exist", "", flagAbbr, true},
+		{"c", "c", command, false},
+		{"a", "a", argument, false},
+		{"vd", "vd", valueDefault, false},
+		{"vf", "vf", valueFixed, false},
+		{"v", "v", value, false},
+		{"f", "f", flag, false},
+	}
+	defs := testDefs()
+	for _, tt := range tests {
+		t.Run(tt.token, func(t *testing.T) {
+			expToken, err := expandAbbr(tt.token, tt.tokenType, defs)
+			if expToken != tt.expToken {
+				t.Error("unexpected expanded token")
+			}
+			if (err != nil && !tt.errorExp) || (err == nil && tt.errorExp) {
+				t.Error("unexpected error result")
+			}
+		})
+	}
+}
