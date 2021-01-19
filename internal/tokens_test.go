4a44c56d243b7012f0fd799c606a09ab3badf604
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2021-01-18 22:12:33 -0800

    Tests fix

--- internal/tokens_test.go
+++ internal/tokens_test.go
@@ -53,24 +53,20 @@ func TestExpandAbbr(t *testing.T) {
 		token     string
 		expToken  string
 		tokenType int
-		expError  bool
 	}{
-		{"c1", "command1", command, false},
-		{"command-abbr-that-doesnt-exist", "", command, true},
-		{"a1", "argument1", argument, false},
-		{"argument-abbr-that-doesnt-exist", "", argument, true},
-		{"c", "c", command, true},
-		{"a", "a", argument, true},
-		{"v", "v", value, false},
+		{"c1", "command1", command},
+		{"command-abbr-that-doesnt-exist", "command-abbr-that-doesnt-exist", command},
+		{"a1", "argument1", argument},
+		{"argument-abbr-that-doesnt-exist", "argument-abbr-that-doesnt-exist", argument},
+		{"c", "c", command},
+		{"a", "a", argument},
+		{"v", "v", value},
 	}
 	defs := mockDefinitions()
 	for _, tt := range tests {
 		t.Run(tt.token, func(t *testing.T) {
-			expToken, err := expandAbbr(tt.token, tt.tokenType, defs)
-			if err == nil {
-				assertValEquals(t, expToken, tt.expToken)
-			}
-			assertError(t, err, tt.expError)
+			expToken := expandAbbr(tt.token, tt.tokenType, defs)
+			assertValEquals(t, expToken, tt.expToken)
 		})
 	}
 }
