83e0b8b0ce37f608721a49d7a04f80c392a8950a
Bogdan Brinza <Bogdan.Brinza@gmail.com>
2021-04-08 15:46:05 -0700

    Reverting test change done earlier to mitigate failure

--- parse_test.go
+++ parse_test.go
@@ -15,18 +15,16 @@ func TestDefinitionsParse(t *testing.T) {
 	}{
 		{[]string{"", "command1", "--argument1"}, "command1", "argument1", 1, false},
 		{[]string{"--argument1"}, "command1", "argument1", 1, false},
-		{[]string{"unknown-token"}, "command1", "argument1", 1, true},
+		{[]string{"unknown-token"}, "command1", "argument1", 1, false},
 	}
 	for ii, tt := range tests {
 		t.Run(strconv.Itoa(ii), func(t *testing.T) {
 			defs := mockDefinitions()
 			req, err := defs.Parse(tt.args)
 			assertError(t, err, tt.expErr)
-			if req != nil {
-				assertValEquals(t, req.Command, tt.expCmd)
-				assertValEquals(t, req.lastArgument, tt.expLastArg)
-				assertValEquals(t, len(req.Arguments), tt.expLenArgs)
-			}
+			assertValEquals(t, req.Command, tt.expCmd)
+			assertValEquals(t, req.lastArgument, tt.expLastArg)
+			assertValEquals(t, len(req.Arguments), tt.expLenArgs)
 		})
 	}
 }
